ae25d444ea21d5035695bcee0cf62c452a91a7ba
purdialex <128404830+purdialex@users.noreply.github.com>
2024-11-05 15:01:32 +0200

    Added the Spring Boot Backend application
    It only has the basic CRUD operations on the User Class,
    ToDo: add all the other classes and link them together

---
+++ backend/MSAProjectWorks/src/test/java/com/example/MSAProjectWorks/MsaProjectWorksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MSAProjectWorks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaProjectWorksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
