9c1d94784c659c48b52d6f59cef3a19e27d7494c
CloudTTQ <claudiutartan@gmail.com>
2024-11-09 19:18:45 +0200

    Added frontend folder and project

---
+++ frontend/app/_layout.jsx
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Stack } from 'expo-router'
+
+import "../global.css";
+
+const _layout = () => {
+  return (
+    <Stack>
+        <Stack.Screen name='index'/>
+    </Stack>
+  )
+}
+
+export default _layout
